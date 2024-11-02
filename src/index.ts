6fe0c13e3e3d9e9a59883640e3d4cdedc1260334
DevJoaoPeu <joaocanaldroid@gmail.com>
2024-11-02 19:04:00 -0300

    feat: usando filter, map e pipe

--- src/index.ts
+++ src/index.ts
@@ -1,23 +1,27 @@
-import { from, Observable } from "rxjs";
+import { filter, from, interval, map, Observable } from "rxjs";
 
 let numbers = [1,5,10];
-let source = from(numbers);
+let source = from(numbers); // Cria uma instância do observable
 
-let sourceInstace = new Observable<number>((subscriber) => {
+let sourceInstace = new Observable<number>((subscriber) => { // Mesma coisa que o from, mas com a instância do observable
   for (let n of numbers) {
     subscriber.next(n)
   }
   subscriber.complete()
 })
 
-const myOberverJson = {
-  next: (x: number) => console.log(x),
+sourceInstace.pipe( // Passa pelo tubo, serve para fazer transformações
+  map((x: number) => x * 2), // map mapeia od dados
+).subscribe({
+  next: (x: number) => console.log(`Map: ${x}`),
   error: (err: Error) => console.error(err),
   complete: () => console.log('complete') 
-}
+})
 
-function component() {
-  sourceInstace.subscribe(myOberverJson)
-}
-
-component()
+sourceInstace.pipe(
+  filter((x: number) => x <= 5) // filter filtra os dados
+).subscribe({
+  next: (x: number) => console.log(`Filter: ${x}`),
+  error: (err: Error) => console.error(err),
+  complete: () => console.log('complete') 
+})
